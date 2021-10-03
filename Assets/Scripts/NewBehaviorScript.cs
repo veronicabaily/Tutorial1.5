8064d830a54b2d4dd97f28676abc7629e3160d71
veronicabaily <veronicabaily@knights.ucf.edu>
2021-10-03 16:55:35 -0400

    Animation commit

--- Assets/Scripts/NewBehaviorScript.cs
+++ Assets/Scripts/NewBehaviorScript.cs
@@ -11,42 +11,81 @@ public AudioClip musicClipTwo;
 
 public AudioSource musicSource;
 
-// Update is called once per frame
-void Update()
+Animator anim;
+
+void Start()
+
 {
+
+  anim = GetComponent<Animator>();
+
+}
+
+// Update is called once per frame
+
+void Update()
+
+{
+
      if (Input.GetKeyDown(KeyCode.W))
+
         {
+
           musicSource.clip = musicClipOne;
+
           musicSource.Play();
+
+          anim.SetInteger("State", 1);
 
          }
 
      if (Input.GetKeyUp(KeyCode.W))
+
         {
+
           musicSource.Stop();
+
+          anim.SetInteger("State", 0);
 
          }
 
      if (Input.GetKeyDown(KeyCode.R))
+
         {
+
           musicSource.clip = musicClipTwo;
+
           musicSource.Play();
+
+          anim.SetInteger("State", 2);
+
          }
 
      if (Input.GetKeyUp(KeyCode.R))
+
         {
+
           musicSource.Stop();
+
+          anim.SetInteger("State", 0);
 
          }
 
      if (Input.GetKeyDown(KeyCode.L))
+
         {
+
           musicSource.loop = true;
+
          }
 
      if (Input.GetKeyUp(KeyCode.L))
+
         {
+
           musicSource.loop = false;
+
         }
-   }
+
+    }
 }
